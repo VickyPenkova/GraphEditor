4470c6e01be008f71ce13594eac39acd6221a927
Tsvetan Tsvetkov <tsvetan.konstantinov@abv.bg>
2019-05-19 20:42:31 +0300

    Add tag backend plugin runner. Add a plugin for NLP Parts of language tagger

---
+++ tag-backend/plugins/pos-tagger-plugin/main.py
@@ -0,0 +1,69 @@
+import nltk
+import argparse
+
+parser = argparse.ArgumentParser(description='Arguments for the tagger')
+
+parser.add_argument('input_text', type=str, help='Text to tag')
+
+def tag(input_text):
+    text = nltk.word_tokenize(input_text)
+
+    tagged = nltk.pos_tag(text)
+    tagged = [(tag[0], LABEL_MAPPINGS[tag[1]]) for tag in tagged]
+
+    return tagged
+
+
+#Source for the meaning of the labels
+#https://medium.com/@gianpaul.r/tokenization-and-parts-of-speech-pos-tagging-in-pythons-nltk-library-2d30f70af13b
+LABEL_MAPPINGS = {
+    'CC': 'coordinating conjunction',
+    'CD': 'cardinal digit',
+    'DT': 'determiner',
+    'EX': 'existential there', #(like: “there is” … think of it like “there exists”)
+    'FW': 'foreign word',
+    'IN': 'preposition/subordinating conjunction',
+    'JJ': 'adjective',
+    'JJR': 'adjective, comparative', #‘bigger’
+    'JJS': 'adjective, superlative', #‘biggest’
+    'LS': 'list marker', #1)
+    'MD': 'modal could', #will
+    'NN': 'noun singular',#‘desk’
+    'NNS': 'noun plural',#‘desks’
+    'NNP': 'proper noun, singular',#‘Harrison’
+    'NNPS': 'proper noun, plural',#‘Americans’
+    'PDT': 'predeterminer',#‘all the kids’
+    'POS': 'possessive ending',#parent’s
+    'PRP': 'personal pronoun',#I, he, she
+    'PRP$': 'possessive pronoun',#my, his, hers
+    'RB': 'adverb',#very, silently
+    'RBR': 'adverb, comparative',#better
+    'RBS': 'adverb, superlative',#best
+    'RP': 'particle',#give up
+    'TO': 'to',#to go ‘to’ the store.
+    'UH': 'interjection',#errrrrrrrm
+    'VB': 'verb',#base form take
+    'VBD': 'verb, past tense',#took
+    'VBG': 'verb, gerund/present participle',#taking
+    'VBN': 'verb, past participle',#taken
+    'VBP': 'verb, sing. present, non-3d',#take
+    'VBZ': 'verb, 3rd person sing. present',#takes
+    'WDT': 'wh-determiner',#which
+    'WP': 'wh-pronoun',#who, what
+    'WP$': 'possessive wh-pronoun',#whose
+    'WRB': 'wh-abverb'#where, when
+}
+
+#TO SETUP THE NLTK DEPENDENCIES RUN THIS:
+
+# import nltk
+# import ssl
+#
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+#
+# nltk.download()
