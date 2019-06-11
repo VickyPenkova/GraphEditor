1b3a64b9495465166b65012abe8dff55d7d2d374
otsvetkov <otsvetkov@vmware.com>
2019-06-11 14:13:31 +0300

    Introduce export to file functionallity

---
+++ ngx-graph-frontend/src/app/core/services/layout.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+
+  private _layoutName: String = 'colaForceDirected';
+  private _layout: Subject<String> = new Subject<String> ();
+
+  private _layouts: any[] = [
+    {
+      label: 'Dagre',
+      value: 'dagre',
+    },
+    {
+      label: 'Dagre Cluster',
+      value: 'dagreCluster',
+      isClustered: true,
+    },
+    {
+      label: 'Cola Force Directed',
+      value: 'colaForceDirected',
+      isClustered: true,
+    },
+    {
+      label: 'D3 Force Directed',
+      value: 'd3ForceDirected',
+    },
+  ];
+
+  constructor() {
+    this._layout.next(this._layoutName);
+  }
+
+  setLayout(name) {
+    this._layout.next(name);
+    this._layoutName = name;
+  }
+
+
+  get layouts(): any[] {
+    return this._layouts;
+  }
+
+  get layoutName(): String {
+    return this._layoutName;
+  }
+
+  get layout():Subject<String> {
+    return this._layout;
+  }
+}
