61822859c13ad025b1ca38b0a0c5b231acc52166
Tsvetan Tsvetkov <tsvetan.konstantinov@abv.bg>
2019-06-13 16:58:51 +0300

    Clean up

--- ngx-graph-frontend/src/app/select-graph/select-graph.component.ts
+++ ngx-graph-frontend/src/app/select-graph/select-graph.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GraphService, GraphDTO } from '../core/services/graph.service';
 import { ItemEditService } from '../core/services/item-edit.service';
-import { PluginService } from '../core/services/plugin.service';
 
 @Component({
   selector: 'app-select-graph',
